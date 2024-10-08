03d783d31e4dea9e229101d7e98708e6ad7c2171
rifat djibran <rifatdjibran099@gmail.com>
2024-10-08 15:38:13 +0700

    menentukan tahun kabisat dengan kelipatan 400

--- Pemilihan2Percobaan118.java
+++ Pemilihan2Percobaan118.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Pemilihan2Percobaan118 { // Ganti nama class sesuai nomor absen 18
+public class Pemilihan2Percobaan118 { 
     public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
@@ -12,14 +12,18 @@ public class Pemilihan2Percobaan118 { // Ganti nama class sesuai nomor absen 18
         tahun = input.nextInt(); 
 
 
-        if (tahun % 4 == 0) {  // Cek jika kelipatan 4
-            if (tahun % 100 == 0) {  // Jika kelipatan 100, bukan kabisat
-                System.out.println(tahun + " bukan tahun kabisat.");
-            } else {  // Jika tidak kelipatan 100, kabisat
+        if ((tahun % 4) == 0) {  
+            if ((tahun % 100) == 0) {  
+                if ((tahun % 400) == 0) {
+                    System.out.println(tahun + " adalah tahun kabisat.");
+                } else {
+                    System.out.println(tahun + " bukan tahun kabisat.");
+                }
+            } else {  
                 System.out.println(tahun + " adalah tahun kabisat.");
             }
-        } else {  // Tidak kelipatan 4, bukan kabisat
+        } else {  
             System.out.println(tahun + " bukan tahun kabisat.");
-        }
+        }    
     }
 }
