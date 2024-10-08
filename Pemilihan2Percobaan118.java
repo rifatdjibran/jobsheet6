da8486774c7a1d857d3e16fefb5a794a0e753d8a
rifat djibran <rifatdjibran099@gmail.com>
2024-10-08 16:08:48 +0700

    menentukan tahun kabisat dengan kualifikasi kelipatan 100

--- Pemilihan2Percobaan118.java
+++ Pemilihan2Percobaan118.java
@@ -11,19 +11,19 @@ public class Pemilihan2Percobaan118 {
         System.out.print("Masukkan tahun: ");
         tahun = input.nextInt(); 
 
-
         if ((tahun % 4) == 0) {  
+            System.out.println(tahun + " adalah tahun kabisat."); 
+
             if ((tahun % 100) == 0) {  
+                System.out.println(tahun + " adalah kelipatan 100."); 
                 if ((tahun % 400) == 0) {
-                    System.out.println(tahun + " adalah tahun kabisat.");
+                    System.out.println(tahun + " adalah tahun kabisat."); 
                 } else {
-                    System.out.println(tahun + " bukan tahun kabisat.");
+                    System.out.println(tahun + " bukan tahun kabisat."); 
                 }
-            } else {  
-                System.out.println(tahun + " adalah tahun kabisat.");
-            }
+            } 
         } else {  
-            System.out.println(tahun + " bukan tahun kabisat.");
-        }    
+            System.out.println(tahun + " bukan tahun kabisat."); 
+        }
     }
 }
